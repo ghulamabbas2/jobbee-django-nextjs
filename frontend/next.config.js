098f45b1263ecaafc8932541949fd4227adab6b2
Ghulam Abbas <ghulamabbas258@gmail.com>
2022-02-28 01:39:33 +0500

    prepared-for-deploy

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -4,8 +4,6 @@ const nextConfig = {
   env: {
     // API_URL: "http://localhost:8000",
     API_URL: "https://jobbee-django.herokuapp.com",
-    MAPBOX_ACCESS_TOKEN:
-      "pk.eyJ1IjoiZ2h1bGFtYWJiYXMyNTgiLCJhIjoiY2t1bXduZ3ByMDMzcjJ2cXJwbGZvNXdmdiJ9.WSfFtn0Wb0NNlB-IxDd3AA",
   },
 };
 
